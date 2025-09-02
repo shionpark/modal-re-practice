63aa22634146a9f9e5e73df44b2c7091c57d70fb
Shion <seoy1108@gmail.com>
2025-09-02 22:50:46 +0900

    design: 모달 오버레이 추가 및 중앙 정렬

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,16 +11,21 @@ function Modal({ isModalOpen, closeModal, title, children }: ModalProps) {
   if (!isModalOpen) return;
 
   return (
-    <div className="relative flex flex-col gap-4 rounded-xl border border-black p-6">
-      <button
-        className="absolute top-2 right-2 h-[1.8rem] w-[1.8rem] cursor-pointer rounded-full bg-black text-sm text-white hover:opacity-70"
-        onClick={closeModal}
-      >
-        X
-      </button>
-      <h2 className="text-xl font-bold">{title}</h2>
-      <div>{children}</div>
-    </div>
+    <>
+      <div className="modal-overlay fixed inset-0 bg-black opacity-50" />
+      <div className="modal-container fixed inset-0 z-20 flex items-center justify-center">
+        <div className="modal-body relative flex flex-col gap-4 rounded-xl bg-white p-6">
+          <button
+            className="absolute top-2 right-2 h-[1.8rem] w-[1.8rem] cursor-pointer rounded-full bg-black text-sm text-white hover:opacity-70"
+            onClick={closeModal}
+          >
+            X
+          </button>
+          <h2 className="text-xl font-bold">{title}</h2>
+          <div>{children}</div>
+        </div>
+      </div>
+    </>
   );
 }
 
