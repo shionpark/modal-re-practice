dbbdc580d361185dff24549f722b91c763f33965
Shion <seoy1108@gmail.com>
2025-09-02 22:45:31 +0900

    design: 모달 및 버튼 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,44 @@ function App() {
 
   return (
     <>
-      <button onClick={openModal}>모달 열기</button>
-      <button onClick={closeModal}>모달 닫기</button>
-      <Modal isModalOpen={isOpen} />
+      <button
+        className="cursor-pointer rounded-xl border border-gray-300 bg-gray-200 px-4 py-3 text-black hover:border-blue-500"
+        onClick={openModal}
+      >
+        모달 열기
+      </button>
+      <button
+        className="cursor-pointer rounded-xl border border-gray-300 bg-gray-200 px-4 py-3 text-black hover:border-blue-500"
+        onClick={closeModal}
+      >
+        모달 닫기
+      </button>
+
+      <Modal isModalOpen={isOpen} closeModal={closeModal} title="Alerts!">
+        <div className="relative">
+          <p className="mb-4">정보 제공에 동의하십니까?</p>
+          <div className="flex justify-between">
+            <button
+              className="cursor-pointer rounded-xl border border-gray-300 bg-gray-200 px-2 py-1 text-black hover:border-blue-500"
+              onClick={() => {
+                confirm('동의하였습니다.');
+                closeModal();
+              }}
+            >
+              예
+            </button>
+            <button
+              className="cursor-pointer rounded-xl border border-gray-300 bg-gray-200 px-2 py-1 text-black hover:border-blue-500"
+              onClick={() => {
+                confirm('거부하였습니다.');
+                closeModal();
+              }}
+            >
+              아니오
+            </button>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,27 @@
+import type { ReactNode } from 'react';
+
 interface ModalProps {
   isModalOpen: boolean;
+  closeModal: () => void;
+  title: string;
+  children: ReactNode;
 }
 
-function Modal({ isModalOpen }: ModalProps) {
+function Modal({ isModalOpen, closeModal, title, children }: ModalProps) {
   if (!isModalOpen) return;
 
-  return <div className="border bg-black p-2 text-amber-300">Modal</div>;
+  return (
+    <div className="relative flex flex-col gap-4 rounded-xl border border-black p-6">
+      <button
+        className="absolute top-2 right-2 h-[1.8rem] w-[1.8rem] cursor-pointer rounded-full bg-black text-sm text-white hover:opacity-70"
+        onClick={closeModal}
+      >
+        X
+      </button>
+      <h2 className="text-xl font-bold">{title}</h2>
+      <div>{children}</div>
+    </div>
+  );
 }
 
 export default Modal;
